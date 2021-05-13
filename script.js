9c1b3bf87c17c4696f2965097f8cd1f88a942fe8
Aleksei Sokolov <aleksei.sokolov.85@mail.ru>
2021-05-13 20:41:30 +0300

    fixes

--- script.js
+++ script.js
@@ -2,42 +2,62 @@
 let input = document.querySelector('.container-check');
 let button = document.querySelector('.button');
 let inputAll = document.querySelector('.container-change')
+let taskContainer = document.querySelector('.container-tasks');
 let tasks =[]; 
 
 button.addEventListener('click', () => {
-        if(input.value !== '') {        
-            let div = document.createElement('div');
-            div.classList.add('li')
-            div.textContent = input.value
-            //console.log(input.value)
-            tasks.push(input.value)
-            console.log(tasks)
+    if(input.value !== '') {        
+        let div = document.createElement('div');
+        div.classList.add('li')
+        div.textContent = input.value
+        //console.log(input.value)
+        tasks.push(input.value)
+        console.log(tasks)
 
-            refreshTasks(tasks, div)
+        refreshTasks(tasks)
 
-            input.value = "";
-        }
+        input.value = "";
+    }
 });
 
-    function refreshTasks(tasks, div) {
-        tasks.forEach((item) => { //перебираем массив
-            div.textContent = item //создаем строки с вводом
-            inputAll.append(div)
-            let close = document.createElement('img') // ставим крестик у каждого div
-            close.classList.add('close')
-            close.src = 'images/Group56kre.svg' 
-            div.append(close)
+function refreshTasks(tasks) {
+    taskContainer.innerHTML = ''
 
-            close.addEventListener('click', () => { 
-                div.remove();
+    tasks.forEach((item) => { //перебираем массив
+        let div = document.createElement('div');
+        div.textContent = item //создаем строки с вводом
+        taskContainer.append(div)
+        let close = document.createElement('img') // ставим крестик у каждого div
+        close.classList.add('close')
+        close.src = 'images/Group56kre.svg' 
+        div.append(close)
+
+        close.addEventListener('click', () => { 
+            tasks = tasks.filter((item) => {
+                return div.textContent !== item;
+            });
+            console.log(tasks);
+            refreshTasks(tasks);
         });
-    
+        
     })    
 }
+
+function sortTasks() {
+    // 1. Cортировка массива tasks
+
+    // 2. refreshTasks(tasks)
+}
+
+
 /*let sort = document.querySelector('.container-list')
 sort.addEventListener('click', (ev) => {
     if()
 })*/
 
-
+// tasks = [
+//     'КУпить рыбу',
+//     'Почистить зубы',
+//     'Сходить в магазин'
+// ]
 
