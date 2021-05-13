5a94498e552b9a82d3dca91a5cae52d1b739b391
Aida IdriSova <aida-107@mail.ru>
2021-05-13 13:48:30 +0300

    script1

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
 
-//получить содержимое от инпута
-let input = document.querySelectorAll('.container-check');
+let input = document.querySelector('.container-check');
 let button = document.querySelector('.button');
-
+let tasks =[]; //массив из эллементов ли со значениями
 
 button.addEventListener('click', () => {
-    input.forEach((item) => {
-        console.log(item.value); // содержимое ввода
-        if(item.value !== '') {           //если содержимое ввода заполнено, создаем ли и очищаем инпут
+        if(input.value !== '') {        
             let li = document.createElement('li');
-            li.textContent = item.value
-            let list = document.querySelector('ul');
-            list.appendChild(li)
-            //очищаем инпут
-            item.value = "";
+            li.textContent = input.value
+            //console.log(input.value)
+            tasks.push(input.value)
+            console.log(tasks)
+            refreshTasks(tasks, li)
+
+            input.value = "";
         }
-    })
 });
 
+function refreshTasks(tasks, li) {
+        let list = document.querySelector('.list');
+        
+        tasks.forEach((item) => { //перебираем массив
+            li.textContent = item //создаем строки с вводом
+            list.appendChild(li)
+          //  input.append(list)
+            let close = document.createElement('img') // ставим крестик у каждого li
+            close.classList.add('close')
+            close.src = 'images/Group56kre.svg' 
+            li.append(close)
+
+            close.addEventListener('click', () => { 
+                li.remove();
+            });
+            close.addEventListener('mouseover', (event) => {
+               // event.target.close.classList.add(':hover')
+            });
+        })    
+}
+
+
+/*
+function changeColor(event) {
+    event.target.style.color = '#833AE0'
+};
+function getRemove(event) {
+    event.target.li.remove()
+};
+close.forEach((element) => {
+    element.addEventListener('mouseover', changeColor);
+    element.addEventListener('click', getRemove);
+});*/
