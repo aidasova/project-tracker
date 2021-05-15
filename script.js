9013d024f174def711383205f6b22f5f71942292
Aleksei Sokolov <aleksei.sokolov.85@mail.ru>
2021-05-15 11:06:49 +0300

    sort fixes

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let button = document.querySelector('.button');
 let taskContainer = document.querySelector('.container-tasks');
 let sort = document.querySelector('.container-list');
 let tasks =[]; 
+let sortFlag = true;
+
+sort.addEventListener('click', function() {
+    console.log(tasks, 'sort');
+    sortTasks(tasks);
+});
+
 
 button.addEventListener('click', () => {
     if(input.value !== '') {        
@@ -11,6 +18,7 @@ button.addEventListener('click', () => {
         div.classList.add('li')
         div.textContent = input.value
         //console.log(input.value)
+        console.log(tasks, 'add');
         tasks.push(input.value)
         console.log(tasks)
 
@@ -19,38 +27,16 @@ button.addEventListener('click', () => {
     }
 });
 
-function refreshTasks(tasks) {
-    taskContainer.innerHTML = ''
-
-    tasks.forEach((item) => { //перебираем массив
-        let div = document.createElement('div');
-        div.textContent = item //создаем строки с вводом
-        taskContainer.append(div)
-        let close = document.createElement('img') // ставим крестик у каждого div
-        close.classList.add('close')
-        close.src = 'images/Group56kre.svg' 
-        div.append(close)
-
-        close.addEventListener('click', () => { 
-            tasks = tasks.filter((item) => {
-                return div.textContent !== item;
-            });
-            console.log(tasks);
-            refreshTasks(tasks);
-        });
-        
-    })    
-}
 
 function clickImage () {
     sort.addEventListener('mouseover', () => {
-        sort.src = (sort.src == 'images/strUpgrey.svg') ? 'images/strDownGrey.svg': 'images/StrDownblack.svg';
+        sort.src = (sort.src == window.location.origin + '/images/strUpgrey.svg') ? 'images/strDownGrey.svg': 'images/StrDownblack.svg';
      
     });
 }
-    sort.addEventListener('click', () => { 
-        sort.src = (sort.src == 'images/StrDownblack.svg') ? 'images/strUpgrey.svg' : 'images/StrUpBlack.svg'
-    });
+    // sort.addEventListener('click', () => { 
+    //     sort.src = (sort.src == window.location.origin + '/images/strDownblack.svg') ? 'images/strUpgrey.svg' : 'images/StrUpBlack.svg'
+    // });
   /*  sort.addEventListener('mouseout', () => {
         sort.src = (sort.src == 'images/strDownblack.svg') ? 'images/StrUpgrey.svg' : 'images/strDownGrey.svg'; 
     })*/
@@ -62,34 +48,60 @@ function clickImage () {
 //}
 
 
-
 function sortTasks(tasks) {
-    sort.addEventListener('click', () => {
-        if(sort.src = 'images/strDownblack.svg') {
 
-            tasks.sort((a, b) => {
-                if(a > b) {
-                    return -1;
-                } if(b > a) {
-                    return 1;
-                }
-                
-            });
-        sort.src = 'images/strUpBlack.svg'
-        } if(sort.src = 'images/strUpBlack.svg') {
-            tasks.sort((a, b) => {
-                if(a < b) {
-                    return -1;
-                } if(b < a) {
-                    return 1;
-                }
-                
-            });
-        sort.src = 'images/strDownblack.svg'
+    if(sortFlag === true) {
         
+        tasks.sort((a, b) => {
+            if(a > b) {
+                return -1;
+            } if(b > a) {
+                return 1;
+            }
+            
+        });
         
-        }
-        refreshTasks(tasks)
-    });
+        sortFlag = false;
+    } else {
+        tasks.sort((a, b) => {
+            if(a < b) {
+                return -1;
+            } if(b < a) {
+                return 1;
+            }
+            
+        });
+        sortFlag = true;
+    
+    }
+    refreshTasks(tasks)
 }
-sortTasks(tasks);
+
+function addRemoveListener(div) {
+
+    tasks = tasks.filter((item) => {
+        return div.textContent !== item;
+    });
+
+    console.log(tasks);
+    refreshTasks(tasks);
+}
+
+function refreshTasks(tasks) {
+    taskContainer.innerHTML = ''
+    console.log(tasks);
+    tasks.forEach(function (item) { //перебираем массив
+        console.log(this);
+        let div = document.createElement('div');
+        div.textContent = item //создаем строки с вводом
+        taskContainer.append(div)
+        let close = document.createElement('img') // ставим крестик у каждого div
+        close.classList.add('close')
+        close.src = 'images/Group56kre.svg' 
+        div.append(close)
+
+        close.addEventListener('click', function() { 
+            addRemoveListener(div);
+        });
+    })    
+}
